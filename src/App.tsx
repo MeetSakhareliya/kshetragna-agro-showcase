f06ae6a82c64ed0082eb531048167e31718b1b4d
Meet Sakhareliya <meetsakhareliya4702@gmail.com>
2025-10-28 13:02:38 +0530

    about route change

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/kshetragna-agro-showcase/about" element={<AboutUs />} />
+          <Route path="/about" element={<AboutUs />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<Index />} />
         </Routes>
