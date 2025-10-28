1bd17f82630c63efbaa9d59a2aef7407a6d78add
Meet Sakhareliya <meetsakhareliya4702@gmail.com>
2025-10-28 13:06:54 +0530

    navigation changed

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -21,7 +21,7 @@ export const Navigation = () => {
     { label: "Home", id: "home", type: "link" },
     { label: "Equipment", id: "products", type: "scroll" },
     { label: "Videos", id: "videos", type: "scroll" },
-    { label: "About Us", id: "/kshetragna-agro-showcase/about", type: "link" },
+    { label: "About Us", id: "/about", type: "link" },
     { label: "Testimonials", id: "testimonials", type: "scroll" },
     { label: "Contact", id: "contact", type: "scroll" },
   ];
